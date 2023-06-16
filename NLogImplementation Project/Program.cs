2cf7fa154046dc9fe4900423fbfc3e4f376c1846
Poojachakor <chakorpooja07@gmail.com>
2023-06-16 21:43:47 +0530

    Add: UC3 file

--- NLogImplementation Project/Program.cs
+++ NLogImplementation Project/Program.cs
@@ -6,11 +6,11 @@ namespace NLogImplementation_Project
     {
         static void Main(string[] args)
         {
-            ArrayListOperation arrayListOperation = new ArrayListOperation();
-            arrayListOperation.AddItems();
-            arrayListOperation.ReadItems();
-            arrayListOperation.UpdateItems();
-            arrayListOperation.DeleteItem();
+            DictionaryOperations dictionaryOperations = new DictionaryOperations();
+            dictionaryOperations.AddItem();
+            dictionaryOperations.ReadItem();
+            dictionaryOperations.UpdateItem();
+            dictionaryOperations.DeleteItem();
         }
     }
 }
